238c7f27ae568dfd2a83d702a7d73ecd46a493fd
Kirill Lashuk <kl7.666@gmail.com>
2010-04-04 18:16:20 +0000

    Last.fm API info moved to separate file lastfm_api_info.py

--- config.py
+++ config.py
@@ -1,19 +1,25 @@
-#!usr/bin/env python
+﻿#!usr/bin/env python
 # -*- coding: utf-8 -*-
 # author: Kirill Lashuk
 
+import lastfm_api_info
 from libs import pylast
 
 # Application constants
 
 DEBUG = True
 
-LASTFM_API_KEY = '0429cda1804dd4d7b1922aa90a1af646'
+# Last.fm api_key stored in lastfm_api_info
+LASTFM_API_KEY = lastfm_api_info.API_KEY
 
+# Final TopArt width
 ABOUT_ME_WIDTH = 300
 
+# Default fetched covers size
 COVER_SIZE = pylast.COVER_SMALL 
 
-EXPIRATION_TIME = 4 * 24 * 3600 # 4 day in seconds
+# Memcache expiration time
+EXPIRATION_TIME = 0     # 4 * 24 * 3600 is 4 day in seconds
 
-BATCH_PUT_LIMIT = 300 # Actual quota is 500 entities
+# Batch db.put limit of entities
+BATCH_PUT_LIMIT = 300   # Actual quota is 500 entities
